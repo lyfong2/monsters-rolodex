2cb6f13af312de872b57e56eed5be3d640ab4a79
Danny Lin <dannyjabez@gmail.com>
2020-10-31 12:45:39 -0400

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import React, { Component } from 'react';
 import './App.css';
 
@@ -7,7 +8,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
   componentDidMount(){
@@ -16,10 +18,23 @@ class App extends Component {
       .then(users => this.setState({ monsters: users}));
   }
   
+  handleChange = (e) =>{
+    this.setState({ searchField: e.target.value });
+  }
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />          
+        <h1>Monsters Rolodex</h1>
+        <SearchBox 
+          placeholder='search monsters'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />          
       </div>
     );
   }  
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card } from '../card/card.component'
 import './card-list.styles.css';
 
+// each component does one thing, and one think very well
 export const CardList = props => (
     <div className='card-list'>
         {props.monsters.map(monster => (
